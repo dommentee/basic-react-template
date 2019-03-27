3b3a4a61cd22789814f5f37531d952c560baf3df
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-03-26 21:23:07 -0400

    added cokies and cookie filters for delete

---
+++ client/components/Title.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Title extends React.Component<{}, {}> {
+  render() {
+    return(
+      <div>
+      <h1 id="header">Hello Dom</h1>
+      <hr/>
+      </div>
+    )
+  }
+}
+
+export default Title;
--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import './styles.scss';
+import Cookies from "js-cookie";
+import Title from './components/Title';
+
 
 interface IState {//outline of the data
   todoList: string[]//string array = to array
@@ -9,55 +12,73 @@ interface IState {//outline of the data
 }
 
 
-// create App, which is the main react component
-class App extends React.Component<{},IState> {
+// create App, which is the main react component. this is root component
+class App extends React.Component<{},IState> {//component = piece of UI 
   constructor(props) {//
-    super(props); 
-    this.state = {
-      todoList: ['todo 1', 'todo 2'],
+    super(props); //Using props means we expect the parent to be in control of changing the value of props. Using state means we expect the component to be in control of its own state.
+    this.state = {//state = data we want displayed when the componet is rendered
+      todoList: Cookies.getJSON('todoList') ? Cookies.getJSON('todoList') : [], // the input will be added to this array
       todoInputVal: ''
     }
   }
 
-  renderTodoList() {//for each todo display as list item
-    return this.state.todoList.map((todo, index)=> {//map retrurns array of html element
-      return <li key={index}>{todo}</li>//map needs key
-    })
-  } 
-
-  onInputSubmit() {//runs to submit input text
-    let uppDatedTodoList = this.state.todoList;
-    uppDatedTodoList.push(this.state.todoInputVal);
-    this.setState({todoList: uppDatedTodoList});
-    this.setState({todoInputVal: ''});
+  componentWillUpdate(currentState: IState, newState: IState) {
+    if(currentState.todoList !== newState.todoList) {
+      Cookies.set('todoList', newState.todoList)
+    }
   }
 
-  render() {
+  renderTodoList() {//for each todo display as list item
+    return this.state.todoList.map((todo, index)=> {//map return html elements in an array. index = method
+      return <li key={index}>{todo}<button onClick={()=>this.deletTodo(index)}>delete</button></li>//map needs key
+    })
+  } 
+  onInputSubmit() {//method that runs when submit input text
+    let uppDatedTodoList = this.state.todoList;//actual list
+    uppDatedTodoList.push(this.state.todoInputVal);//push = appendChild of the input
+    this.setState({todoList: uppDatedTodoList});//set the todolist array to the input text
+    this.setState({todoInputVal: ''});//set input text to empty afterwards  
+  }
+
+  // deletTodo(todoIndex: number) {
+  //   this.setState({ todoList: this.state.todoList.filter((todo, index) => todoIndex !== index ? todo: null ) }); 
+  // }
+
+  deletTodo(todoIndex: number) {
+    const todoList =this.state.todoList.filter((todo, index) => {
+      return (todoIndex !== index) ? todo : null  
+      // if (todoIndex !== index) {
+      //   return todo
+      // } else {
+      //   return null
+      // }
+    });
+    this.setState({ todoList }); 
+  }
+
+  render() {// responsible for describing what the UI should look like
     return (
-      <div>
+      <div className="main">
         <Title/> {/* call component into app */}
-        <input 
-          type="text" 
-          value={this.state.todoInputVal}//reflect the value of the state
-          onChange={event=> this.setState({todoInputVal: event.target.value})}//sets input value state to value of enetered text
-        />
-        <button onClick={()=>this.onInputSubmit()}>Submit</button>
-        <ul>{this.renderTodoList()}</ul>
+        <div className="input-todo-wrap">
+          <input 
+            type="text" 
+            value={this.state.todoInputVal}//reflect the value of the state
+            onChange={event=> this.setState({todoInputVal: event.target.value})}//sets input value state to value of enetered text
+          />
+          <button onClick={()=>this.onInputSubmit()}>Submit</button>
+        </div>
+        <div className="todo-list">
+          <p>Your current list</p>
+          <hr/>
+          <ul>{this.renderTodoList()}</ul>
+        </div>
       </div>
     );
   }
 }
 
-class Title extends React.Component {//creat the component 
-  render() {
-    return (
-      <div>
-        <h1>Hello Dom</h1>
-        <hr/>
-      </div>
-    );
-  }
-} 
+
 
 ReactDOM.render(<App />, document.getElementById("render-target"));
 //React Dom
